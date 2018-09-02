7d79a8e259622c305554c65238d4d93d431a5a2b
nytomato <czs1037976103@gmail.com>
2018-09-02 17:34:15 +0800

    feat(redux): 使用日志中间件

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,13 +2,13 @@
  * @Author: caozhongshe 
  * @Date: 2018-09-02 14:02:23 
  * @Last Modified by: caozhongshe
- * @Last Modified time: 2018-09-02 17:28:57
+ * @Last Modified time: 2018-09-02 17:33:06
  */
 
 import { createStore, applyMiddleware } from 'redux';
-
+import logger from 'redux-logger'; //中间日志
 import state from '../state'
 
-const store = createStore(state, {}, applyMiddleware())
+const store = createStore(state, {}, applyMiddleware(logger))
 
 export default store
