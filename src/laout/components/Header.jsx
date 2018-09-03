6bcd968023ec1dbe2e80bb0532c8fe35a5822700
nytomato <czs1037976103@gmail.com>
2018-09-03 15:52:44 +0800

    暂时提交

---
+++ src/laout/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentWillMount() { }
+  componentDidMount() { }
+  componentWillReceiveProps() { }
+  componentWillUnmount() { }
+  render() {
+    return (<div className="Header">
+      头部
+    </div>
+    );
+  }
+}
+export default App;
--- src/laout/index.jsx
+++ src/laout/index.jsx
@@ -2,12 +2,14 @@
  * @Author: caozhongshe 
  * @Date: 2018-08-31 11:29:33 
  * @Last Modified by: caozhongshe
- * @Last Modified time: 2018-09-02 16:35:09
+ * @Last Modified time: 2018-09-03 15:46:42
  */
 
 import React, { Component } from 'react';
 
 import './main.scss'
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 class ErrorPage extends Component {
   //初始化props
@@ -33,15 +35,12 @@ class ErrorPage extends Component {
   render() {
     return (
       <div className="Laout">
-        <div className="header">
-          头部
-        </div>
-
-        <div>
-          {this.props.children}
-        </div>
-        <div className="footer">
-          底部
+        <Header></Header>
+        <div className="content">
+          <Sidebar></Sidebar>
+          <div className="view">
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
